8887a805b60553cd843d8e60fb1178b383c3a325
1063-DS-Kalantari <l.aghdam@gmail.com>
2022-08-01 10:30:06 -0500

    Create Page.js

---
+++ delicious/src/components/Page.js
@@ -0,0 +1,15 @@
+
+import Home from "./Home"  
+import React from 'react'
+
+
+function pages() {
+    return (
+      <div>
+        <Home />
+      </div>
+    );
+  }
+  
+  export default pages;
+  
